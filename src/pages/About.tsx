1af984348fe9726dbed3c387ea057fc11f948123
sumanth-appu <sumanth-appu>
2024-06-11 07:30:54 +0000

    First Commit from Locofy Builder

---
+++ src/pages/About.tsx
@@ -0,0 +1,213 @@
+import { FunctionComponent } from "react";
+import styles from "./About.module.css";
+
+const About: FunctionComponent = () => {
+  return (
+    <div className={styles.about}>
+      <div className={styles.cardSmall}>
+        <b className={styles.projects}>Projects</b>
+        <div className={styles.copy}>
+          <div className={styles.movieFinderWebsiteContainer}>
+            <ul className={styles.movieFinderWebsite}>
+              <li>MOVIE FINDER WEBSITE:</li>
+            </ul>
+          </div>
+          <div className={styles.descriptionengineeredAWebsit}>
+            DescriptionEngineered a website dedicated to movie discovery,
+            allowing users to search and explore films Incorporated intuitive
+            search functionality, detailed movie information display, and a
+            user-friendly interface. Demonstrates expertise in web development,
+            API integration, and user experience optimization.
+          </div>
+        </div>
+        <div className={styles.copy1}>
+          <div className={styles.movieFinderWebsiteContainer}>
+            <ul className={styles.movieFinderWebsite}>
+              <li>GENEBAGS [Online Bag shopping system]:</li>
+            </ul>
+          </div>
+          <div className={styles.descriptionengineeredAWebsit}>
+            Led the development of GENEBAGS, an innovative online bag shopping
+            platform, enhancing user experience with an intuitive interface,
+            comprehensive product catalog, and streamlined shopping cart.
+            Collaborated on GENEBAGS with a partner, emphasizing teamwork and
+            project accomplishment.
+          </div>
+        </div>
+        <div className={styles.copy}>
+          <div className={styles.movieFinderWebsiteContainer}>
+            <ul className={styles.movieFinderWebsite}>
+              <li>WEB-SOCKET CHAT APP:</li>
+            </ul>
+          </div>
+          <div className={styles.ledTheDevelopment1}>
+            Led the development of GENEBAGS, an innovative online bag shopping
+            platform, enhancing user experience with an intuitive interface,
+            comprehensive product catalog, and streamlined shopping cart.
+            Collaborated on GENEBAGS with a partner, emphasizing teamwork and
+            project accomplishment.
+          </div>
+        </div>
+        <div className={styles.button}>
+          <div className={styles.visitGithub}>Visit GitHub</div>
+        </div>
+      </div>
+      <div className={styles.paragraphWithHeadingAndSub}>
+        <div className={styles.services}>Services:</div>
+        <div className={styles.paragraphWithIconParent}>
+          <div className={styles.paragraphWithIcon}>
+            <div className={styles.illustrations}>Illustrations:</div>
+            <div className={styles.logoCharacterContainer}>
+              <p className={styles.logo}>{`>> Logo`}</p>
+              <p className={styles.logo}>{`>> Character Design`}</p>
+              <p className={styles.logo}>{`>> Animations`}</p>
+            </div>
+          </div>
+          <div className={styles.paragraphWithIcon1}>
+            <div className={styles.illustrations}>Designing:</div>
+            <div className={styles.logoCharacterContainer}>
+              <p className={styles.logo}>{`>> Landing pages`}</p>
+              <p className={styles.logo}>{`>> User flow`}</p>
+              <p className={styles.logo}>{`>> Prototyping`}</p>
+            </div>
+          </div>
+          <div className={styles.paragraphWithIcon2}>
+            <div className={styles.illustrations}>Web-Development:</div>
+            <div className={styles.logoCharacterContainer}>
+              <p className={styles.logo}>{`>> HTML/CSS`}</p>
+              <p className={styles.logo}>{`>> JavaScript`}</p>
+              <p className={styles.logo}>{`>> React,js`}</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.primaryButton}>
+          <div className={styles.visitGithub}>Know More</div>
+        </div>
+      </div>
+      <div className={styles.headline}>
+        <div className={styles.property1default}>
+          <div className={styles.tabListParent}>
+            <div className={styles.tabList}>
+              <div className={styles.python}>
+                <div className={styles.python1}>Python</div>
+              </div>
+              <div className={styles.python2}>
+                <div className={styles.python1}>C/C++</div>
+              </div>
+              <div className={styles.python4}>
+                <div className={styles.python1}>JavaScript</div>
+              </div>
+              <div className={styles.python4}>
+                <div className={styles.python1}>Blockchain</div>
+              </div>
+              <div className={styles.python4}>
+                <div className={styles.python1}>Git/Github</div>
+              </div>
+            </div>
+            <div className={styles.tabList1}>
+              <div className={styles.python10}>
+                <div className={styles.python1}>HTML/CSS</div>
+              </div>
+              <div className={styles.python12}>
+                <div className={styles.python1}>BootStrap</div>
+              </div>
+              <div className={styles.python14}>
+                <div className={styles.python1}>SQL</div>
+              </div>
+              <div className={styles.python16}>
+                <div className={styles.python1}>Google-collab</div>
+              </div>
+              <div className={styles.python18}>
+                <div className={styles.python1}>SASS</div>
+              </div>
+            </div>
+            <div className={styles.tabList2}>
+              <div className={styles.python20}>
+                <div className={styles.python1}>VS Code</div>
+              </div>
+              <div className={styles.python22}>
+                <div className={styles.python1}>PyCharam</div>
+              </div>
+            </div>
+            <b className={styles.skills}>Skills</b>
+          </div>
+          <b className={styles.sumanthAkshali}>Sumanth Akshali</b>
+          <b className={styles.bengaluruIndia}>
+            Bengaluru, India | Ph no: +91 8123367434
+          </b>
+          <b className={styles.sumanthAppugithubioMail}>
+            sumanth-appu.github.io | Mail: sumanthappu@outlook.com
+          </b>
+          <div className={styles.property1defaultChild} />
+          <div className={styles.passionateAboutStaying}>
+            Passionate about staying abreast of cutting-edge technologies and
+            eager to contribute to real-world projects while enhancing my
+            expertise. Seeking opportunities to further my knowledge and gain
+            hands-on experience in emerging technologies. Committed to
+            continuous learning and growth in a dynamic tech environment.
+          </div>
+          <div className={styles.secondaryButton}>
+            <div className={styles.visitGithub}>Expand</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.associateCloudEngineerGoogl}>
+        <div className={styles.associateCloudEngineerContainer}>
+          <p className={styles.logo}>
+            • Associate cloud Engineer Google Cloud skill Boost
+          </p>
+          <p
+            className={styles.logo}
+          >{`• IBM course on web-dev (Credly badge) Coursera Certification `}</p>
+          <p className={styles.logo}>
+            • Python for Data Science Infosys Springboard
+          </p>
+          <p className={styles.logo}>
+            • C programming for beginners Udemy Certification
+          </p>
+          <p className={styles.logo}>
+            • Getting Started with Git and GitHub Coursera Certification
+          </p>
+        </div>
+      </div>
+      <div className={styles.blockchainAndItsApplication}>
+        <div className={styles.associateCloudEngineerContainer}>
+          • Blockchain and its Application| IIT Kharagpur NPTEL Certification
+        </div>
+      </div>
+      <div className={styles.certifications}>
+        <b className={styles.associateCloudEngineerContainer}>CERTIFICATIONS</b>
+      </div>
+      <div className={styles.headerNavigation}>
+        <div className={styles.items}>
+          <div className={styles.button1}>
+            <div className={styles.visitGithub}>Blog</div>
+          </div>
+          <div className={styles.visitGithub}>Service</div>
+          <div className={styles.visitGithub}>Contact</div>
+        </div>
+      </div>
+      <div className={styles.frame}>
+        <b className={styles.education}>Education</b>
+        <div className={styles.bachelorOfEngineeringInInf}>
+          <div className={styles.bachelorOfEngineeringContainer}>
+            <p className={styles.logo}>
+              Bachelor of Engineering in Information Science, Acharya Institute
+              of Technology | 2020 – 2024 | CGPA: 7.89
+            </p>
+            <p className={styles.logo}>
+              Pre-University, KLE’s Prerana PU Collage, Hubli | 2018-2020 |
+              Percentage: 86.67%
+            </p>
+            <p className={styles.logo}>
+              Class-10, Prasiddhi High School, Hagaribommanahalli | 2018 |
+              Percentage: 95.56%
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
